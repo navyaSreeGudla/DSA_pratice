d2f054044768a9a866474e3bf876a382b2f05283
navyaSreeGudla <162750447+navyaSreeGudla@users.noreply.github.com>
2026-06-10 13:02:42 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+
+    void push(int val) {
+        if(st.empty())
+            st.push({val,val});
+        else
+            st.push({val, min(val, st.top().second)});
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
