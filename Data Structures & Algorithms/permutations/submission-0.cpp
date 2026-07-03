61e8708e0fbc74bcd13622796f0118f3f8d87e65
navyaSreeGudla <162750447+navyaSreeGudla@users.noreply.github.com>
2026-07-03 12:26:37 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(int idx,vector<int>& nums,vector<vector<int>>& ans){
+        int n = nums.size();
+        if(idx==n){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = idx;i<n;i++){
+            swap(nums[i],nums[idx]);
+            solve(idx+1,nums,ans);
+            swap(nums[i],nums[idx]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>ans;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
