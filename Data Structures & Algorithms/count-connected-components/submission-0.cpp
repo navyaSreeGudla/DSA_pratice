7db43cdf992a93aa1fda742a1808fbae1ce3a72f
navyaSreeGudla <162750447+navyaSreeGudla@users.noreply.github.com>
2026-07-09 10:20:12 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(int node,vector<vector<int>>& adj ,vector<bool>& vis){
+        if(vis[node]==true){
+            return;
+        }
+        vis[node]=true;
+        for(int neigh:adj[node]){
+            dfs(neigh,adj,vis);
+        }
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+    vector<vector<int>>adj(n);
+     for(auto &e:edges){
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+     }
+    vector<bool>vis(n,false);
+    int cnt =0;
+    for(int i =0;i<n;i++){
+        if(vis[i]==false){
+            dfs(i,adj,vis);
+            cnt++;
+        }
+    }
+    return cnt;
+    }
+};
